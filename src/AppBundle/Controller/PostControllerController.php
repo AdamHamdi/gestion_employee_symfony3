8b521558d6d3c2f1cc4882617582324b324d79e1
Adam Hamdi <prodev.webdesign@gmail.com>
2020-12-03 16:43:56 +0100

    PostController function add post

--- src/AppBundle/Controller/PostControllerController.php
+++ src/AppBundle/Controller/PostControllerController.php
@@ -2,9 +2,28 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Post;
+use AppBundle\Form\PostType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostControllerController extends Controller
 {
+     /**
+     * @Route("/post/add", name="post.new")
+     */
+    public function newPost(Request $request){
+
+        $post= new Post();
+        $form=$this->createForm(PostType::class,$post);
+        $form->add('title')->add('description');
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager=$this->getDoctrine()->getManager();
+            $this-> entityManager->persist($post);
+            $this-> entityManager->flush();
+        }
+        return $this->render('blog/add.html.twig',['form'=>$form->createView()]) ;
+    }
 }
