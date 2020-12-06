59b88d45a9c28c99f5c8bbb57866013454c5d085
Adam Hamdi <prodev.webdesign@gmail.com>
2020-12-06 09:44:12 +0100

    login.html.twig

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Doctrine\ORM\EntityManagerInterface;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authentication)
+    {
+        // get the login error if there is one
+    $error = $authentication->getLastAuthenticationError();
+
+    // last username entered by the user
+    $lastUsername = $authentication->getLastUsername();
+    
+    return $this->render('security/login.html.twig',[
+        'last_username'=>$lastUsername,
+        'error'=>$error,
+    ]);
+    }
+    /**
+  * @Route("/logout", name="logout")
+  */
+ public function logout(){
+
+}
+}
